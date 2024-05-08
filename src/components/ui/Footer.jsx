aa675d51729e17fe5b4b47b322cf335b0a87e4d6
benny-sit <108618452+benny-sit@users.noreply.github.com>
2024-05-08 23:07:26 +0300

    added privacy policy

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
@@ -15,6 +16,12 @@ export default function Footer() {
         alt="icon webasics"
         className="relative mt-7 mb-11"
       />
+      <Link
+        href={"/privacy-policy"}
+        className="ml-auto text-w-blue hover:underline mb-1"
+      >
+        מדיניות פרטיות
+      </Link>
       <p className="w-full">© כל הזכויות שמורות ל-webasics 2024</p>
       <div className="w-full bg-w-cream h-1 mt-2 mb-5"></div>
     </footer>
